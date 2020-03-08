8cca5eb4ae79244ff6dd724e0e5b718414a8ec94
DICE777 <seulgi7383@gmail.com>
2020-03-08 23:19:33 +0900

    Android Service Example (Media Player)

--- app/src/main/java/com/example/androidexample/MainActivity.java
+++ app/src/main/java/com/example/androidexample/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.androidexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.androidexample.Service.MyService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        /*
+        * Service : 안드로이드의 4대 컴포넌트 중 하나
+        * 화면 없이 동작함
+        * 보통 Activity 에서 호출되어 시작함
+        *
+        * 1. 사용할 Service (*.java)를 만든다
+        * 2. AndroidManifest.xml 에 Service 를 등록한다.
+        * 3. Service를 시작하도록 호출한다.
+        * */
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 서비스 시작하기
+                Log.d("test", "activity - service 시작 버튼 클릭");
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재 제어권자
+                        MyService.class); // 이동할 컴포넌트
+                startService(intent); // 서비스 시작
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 서비스 종료하기
+                Log.d("test", "activity - service 종료 버튼 클릭");
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        MyService.class);
+                stopService(intent);
+            }
+        });
+    } // end of onCreate
+} // end of class
---
+++ app/src/main/java/com/example/androidexample/Service/MyService.java
@@ -0,0 +1,48 @@
+package com.example.androidexample.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.example.androidexample.R;
+
+// Service 클래스를 구현하려면, Service 를 상속받는다.
+public class MyService extends Service {
+    MediaPlayer mediaPlayer; // 음악 재생을 위한 객체
+
+    /*
+    * Service 객체와 화면단인 Activity 사이에서 통신(데이터를 주고 받을) 떄 사용하는 메서드
+    * 데이터를 전달할 필요가 없으면 return null;
+    * */
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 서비스에서 가장 먼저 호출됨 (최초에 한 번만!)
+        Log.d("test", "서비스의  onCreate");
+        mediaPlayer = MediaPlayer.create(this, R.raw.chacha);
+        mediaPlayer.setLooping(false); // 반복재생
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // 서비스가 호출될 때마다 실
+        Log.d("test", "서비스의 onStartCommand");
+        mediaPlayer.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // 서비스가 종료될 때 실행
+        mediaPlayer.stop();
+        Log.d("test", "서비스의 onDestroy");
+    }
+}
